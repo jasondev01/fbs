0fae96652d206c06c1d7bf454a74d2d1d3c100ca
Jason Ruben <jsnrbn01@gmail.com>
2023-08-06 02:02:11 +0800

    serverless functions to connect mongodb

--- index.js
+++ index.js
@@ -6,14 +6,11 @@ const userRoute = require("./Routes/userRoute");
 const app = express(); // add extra capability
 require("dotenv").config(); 
 
-const corsOptions = {
-    origin: "*", // Ganti dengan URL frontend Anda
-    credentials: true, // Mengizinkan pengiriman cookie melalui CORS
-};
+
 
 // api routes // middlewares
 app.use(express.json()); 
-app.use(cors(corsOptions)); 
+app.use(cors()); 
 app.use("/api/users", userRoute);
 
 
@@ -22,16 +19,63 @@ app.get("/", (req, res) => {
     res.send("Wrong Port");
 });
 
+const corsOptions = {
+    origin: "*", // Ganti dengan URL frontend Anda
+    credentials: true, // Mengizinkan pengiriman cookie melalui CORS
+};
+
+app.use(cors(corsOptions)); 
+
+
 const port = process.env.PORT || 5000;
-const uri = process.env.ATLAS_URI || 5000;
+const uri = process.env.ATLAS_URI;
 
 app.listen(port, (req, res) => {
     console.log(`server running on port: ${port}`);
 });
 
-mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log("MongoDB connection establised"))
-.catch((error) => console.log("MongoDB Connection failed: ", error.message))
+// mongoose.connect(uri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+// .then(() => console.log("MongoDB connection establised"))
+// .catch((error) => console.log("MongoDB Connection failed: ", error.message))
+
+async function connectToMongoDB() {
+    try {
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("MongoDB connected");
+    } catch (error) {
+        console.error("Failed to connect to MongoDB:", error);
+    }
+}
+  
+  // Function to close the MongoDB connection
+async function closeMongoDBConnection() {
+    try {
+        await mongoose.disconnect();
+        console.log("MongoDB connection closed");
+    } catch (error) {
+        console.error("Error while closing MongoDB connection:", error);
+    }
+}
+  
+// API routes
+app.use("/api/users", userRoute);
+  
+// Serverless function handler
+const handler = createServer(app);
+  
+// Invoke MongoDB connection on function start
+connectToMongoDB();
+  
+// Export the serverless function handler
+module.exports = async (req, res) => {
+    const result = await handler(req, res);
+    // Invoke MongoDB connection close after function execution
+    closeMongoDBConnection();
+    return result;
+};
